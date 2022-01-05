bd909501a5a902267e48f77dd644ac6436756ce9
agMalek <albermaleh.99@gmail.com>
2022-01-04 23:05:05 -0300

    entrega

--- src/components/Agregar/Agregar.js
+++ src/components/Agregar/Agregar.js
@@ -1,22 +1,71 @@
+import { useEffect, useState } from 'react';
 import './Agregar.css'
+import {v4 as uuidv4} from 'uuid'
 
 const Agregar = () => {
+        
+    let productos;
+
+
+    /* PARA PODER HACER EL .PUSH LA PRIMERA VEZ QUE AGREGO, EN CASO DE QUE EL SOTRAGE DEVUELVA NULL, INSTANCIO A PRODUCTOS COMO ARRAY VACIO */
+    useEffect(()=> {
+        let storage = JSON.parse(localStorage.getItem("productos"))
+        if( storage === null) {
+            productos = []
+        }
+        else{
+            productos = storage
+        }
+    })
+
+    const [nombre, setNombre] = useState("")
+    const [cantidad, setCantidad] = useState(1)
+    const [precio, setPrecio] = useState(1)
+    
+
+    /* -----------   AGREGO PRODUCTO -------------- */
+    const guardarDatos = (e) => {
+        e.preventDefault()
+        let pro = {
+            id: uuidv4(),
+            nombre: nombre,
+            cantidad: cantidad,
+            precio: precio
+        }
+        productos.push(pro)
+        localStorage.setItem("productos", JSON.stringify(productos))
+        limpiarDatos()
+    }
+    
+    /* --------LIMPIA VALORES PARA LIPMIAR LOS INPUT  */
+    const limpiarDatos = () =>{
+        setNombre("")
+        setCantidad(1)
+        setPrecio(1)
+    }
+
     return (
         <div className='container mt-4'>
+            
             <h2>Agregar producto</h2>
 
-            <form>
+            <form onSubmit={(e) => guardarDatos(e)}>
+                {/*  -------------- CAMPO NOMBRE  -----------*/}
                 <div className="mb-3">
                     <label className="form-label">Nombre del producto</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                 </div>
+
+                {/*  -------------- CAMPO CANTIDAD  -----------*/}
                 <div className="mb-3">
                     <label className="form-label">Cantidad</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" min={1} className="form-control" value={cantidad} onChange={(e) => setCantidad(e.target.value)}/>
                 </div>
+
+                {/*  -------------- CAMPO PRECIO  -----------*/}
                 <div className="mb-3">
                     <label className="form-label">Precio</label>
-                    <input type="number" className="form-control" />
+                    <input type="number" min={1} className="form-control" value={precio} onChange={(e) => setPrecio(e.target.value)}/>
                 </div>
                 <button type="submit" className="btn btn-success">Guardar</button>
             </form>
--- src/components/Productos/Productos.js
+++ src/components/Productos/Productos.js
@@ -1,21 +1,68 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react/cjs/react.development';
 import './Productos.css'
 
 const Agregar = () => {
+
+
+    const [productos, setProductos] = useState(JSON.parse(localStorage.getItem("productos")))
+
+
+    const eliminarItem = (id) => {
+        setProductos(productos.filter(p => id !== p.id ))
+    }
+
+    useEffect(() => {
+        if(productos !== null){
+            localStorage.setItem("productos", JSON.stringify(productos))
+        }
+    }, [productos])
+
+
+    const eliminarTodo = () => {
+        localStorage.clear()
+        setProductos(null)
+    }
+
     return (
         <div className='container mt-4'>
             <h2>Productos</h2>
 
             <Link to="/"><button className="btn btn-success me-3">Agregar +</button></Link>
-            <button className="btn btn-danger">Eliminar todo</button>
+            {/* INHABILITO EL BOTON SI NO HAY PRODUCTOS PARA ELIMINAR*/}
+            <button className="btn btn-danger" disabled={productos === null || productos.length === 0} onClick={() => eliminarTodo()}>Eliminar todo</button>
 
-            <div className='row border-top border-bottom mt-4 p-2'>
-                <div className='col-2 fw-bolder'>#</div>
-                <div className='col-5 fw-bolder'>Nombre del producto</div>
-                <div className='col-2 fw-bolder'>Cantidad</div>
-                <div className='col-2 fw-bolder'>Precio</div>
-                <div className='col-1 fw-bolder'>Acción</div>
-            </div>
+            
+            {
+                productos !== null && productos.length > 0 ?
+
+                /* SI HAY PRODUCTOS */
+                <>
+                    <div className='row border-top border-bottom mt-4 p-2'>
+                        <div className='col-2 fw-bolder'>#</div>
+                        <div className='col-4 fw-bolder'>Nombre del producto</div>
+                        <div className='col-2 fw-bolder'>Cantidad</div>
+                        <div className='col-2 fw-bolder'>Precio</div>
+                        <div className='col-2 fw-bolder'>Acción</div>
+                    </div>
+                
+                    {/* MUESTRO CADA UNO DE LOS PRODUCTOS */}
+                    {productos.map((p, index)=> (
+                        <div className='row' key={index}>
+                            <div className='col-2 fw-bolder'>{index+1}</div>
+                            <div className='col-4 fw-bolder'>{p.nombre}</div>
+                            <div className='col-2 fw-bolder'>{p.cantidad}</div>
+                            <div className='col-2 fw-bolder'>{p.precio * p.cantidad}</div>
+                            <div className='col-2 fw-bolder'> <button className='btn btn-outline-danger' onClick={() => eliminarItem(p.id)}>Eliminar</button></div>
+                        </div>
+                    ))}
+                    
+                </>
+                /* SI NO HAY PRODUCTOS */
+                : <div className='row border-top border-bottom mt-4 p-2'>
+                    <div className='col-12 fw-bolder'>No hay Productos</div>
+                </div>
+            }
         </div>
     );
 }
